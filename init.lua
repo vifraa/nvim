4deaea0525c3335d4a212cecd7be35c49117cc8b
Viktor Franzen <viktor@frnzn.com>
2023-04-07 00:04:29 +0200

    mappings to rename and format with lsp

--- init.lua
+++ init.lua
@@ -365,8 +365,9 @@ local on_attach = function(_, bufnr)
 		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
 	end
 
-	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+	nmap("<leader>cr", vim.lsp.buf.rename, "[R]e[n]ame")
 	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+	nmap("<leader>cf", vim.lsp.buf.format, "[C]ode [F]ormat")
 
 	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
 	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
