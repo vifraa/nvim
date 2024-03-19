a9fa644377566ddcce095e625fa7de4e492bf6ed
Viktor Franzen <viktor@frnzn.com>
2024-03-19 07:54:15 +0100

    fix nvim config issues with updated plugins

--- init.lua
+++ init.lua
@@ -105,10 +105,9 @@ require("lazy").setup({
     -- Add indentation guides even on blank lines
     "lukas-reineke/indent-blankline.nvim",
     -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
+    -- See `:help indent_blankline.txt`,
+    main = "ibl",
     opts = {
-      char = "┊",
-      show_trailing_blankline_indent = false,
     },
   },
 
@@ -293,7 +292,6 @@ require("nvim-treesitter.configs").setup({
     "rust",
     "tsx",
     "typescript",
-    "help",
     "vim",
     "html",
     "java",
